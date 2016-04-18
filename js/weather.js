03bef35e778dc0fc9ce9750546de4f85c1dcfc30
kuwkuw <kuw-kuw@mail.ru>
2016-04-18 13:51:17 +0300

    add details

--- js/weather.js
+++ js/weather.js
@@ -9,21 +9,19 @@ var Weather = (function(){
     }
 
     Weather.prototype.show = function(data){
-        console.log(data)
-
         this._showWeatherOnTenDays(data.channel.item.forecast);
         this._showWeatherOnNow(data);
     };
 
     Weather.prototype._showWeatherOnNow = function(data){
-        var units = data.channel.units;
-        var condition = data.channel.item.condition;
-
         this._nowWeather.innerHTML = '';
-        this._nowWeather.appendChild(descriptionWrp);
+        this._nowWeather.appendChild(this._createDescriptionWrp(data));
+        this._nowWeather.appendChild(this._createConditionWrp(data));
     };
 
-    Weather.prototype._createDescriptionWrp = function(units, condition){
+    Weather.prototype._createDescriptionWrp = function(data){
+        var condition = data.channel.item.condition;
+
         var descriptionWrp = document.createElement('div');
         descriptionWrp.setAttribute('class', 'description-wrp');
 
@@ -43,38 +41,104 @@ var Weather = (function(){
         descriptionWrp.appendChild(description);
         descriptionWrp.appendChild(date);
         descriptionWrp.appendChild(text);
-        descriptionWrp.appendChild(this._createConditionWrp(units, condition));
 
         return descriptionWrp;
     };
 
-    Weather.prototype._createConditionWrp = function(units, condition){
+    Weather.prototype._createConditionWrp = function(data){
         var conditionWrp = document.createElement('div');
+        var detailsWrp = document.createElement('div');
+
+        conditionWrp.setAttribute('class', 'condition-wrp');
+        detailsWrp.setAttribute('class', 'details-wrp');
+
+        conditionWrp.appendChild(this._createNowConditionElement(data));
+        detailsWrp.appendChild(this._createAstronomyElement(data.channel.astronomy));
+        detailsWrp.appendChild(this._createAtmosphereElement(data.channel.atmosphere));
+        detailsWrp.appendChild(this._createWindElement(data));
+        conditionWrp.appendChild(detailsWrp);
+        return conditionWrp
+    };
+
+    Weather.prototype._createNowConditionElement = function(data){
+        var unitsData = data.channel.units;
+        var conditionData = data.channel.item.condition;
+
+        var nowCondition = document.createElement('div');
         var icon = document.createElement('img');
         var temp = document.createElement('span');
         var unit = document.createElement('span');//'°C'
 
-        temp.innerText = condition.temp;
+        nowCondition.setAttribute('class', 'now-condition-wrp');
+
+        temp.innerText = conditionData.temp;
         temp.setAttribute('class', 'temp');
 
-        icon.setAttribute('src', this._getIconUrl(condition.code));
+        icon.setAttribute('src', this._getIconUrl(conditionData.code));
         icon.setAttribute('class', 'icon');
 
-        unit.innerHTML = '&deg;' + units.temperature;
+        unit.innerHTML = '&deg;' + unitsData.temperature;
         unit.setAttribute('class', 'unit');
 
-        conditionWrp.appendChild(icon);
-        conditionWrp.appendChild(temp);
-        conditionWrp.appendChild(unit);
+        nowCondition.appendChild(icon);
+        nowCondition.appendChild(temp);
+        nowCondition.appendChild(unit);
 
-        return conditionWrp
+        return nowCondition;
     };
 
-    Weather.prototype._createAstronomyElement = function(){};
+    Weather.prototype._createAstronomyElement = function(astronomyData){
+        var astronomyElement = document.createElement('div');
+        var sunrise = document.createElement('div');
+        var sunset = document.createElement('div');
 
-    Weather.prototype._createAtmosphereElement = function(){};
+        astronomyElement.setAttribute('class', 'astronomy-wrp');
+        sunrise.setAttribute('class', 'sunrise');
+        sunrise.innerText = 'Sunrise: ' + astronomyData.sunrise;
+        sunset.setAttribute('class', 'sunset');
+        sunset.innerText = 'Sunset: ' + astronomyData.sunset;
 
-    Weather.prototype._createWindElement = function(){};
+        astronomyElement.appendChild(sunrise);
+        astronomyElement.appendChild(sunset);
+        return astronomyElement;
+    };
+
+    Weather.prototype._createAtmosphereElement = function(atmosphereData){
+        var atmosphereElement = document.createElement('div');
+        var visibility = document.createElement('div');
+        var humidity = document.createElement('div');
+        var pressure = document.createElement('div');
+
+        atmosphereElement.setAttribute('class', 'atmosphere-wrp');
+        visibility.setAttribute('class', 'visibility');
+        humidity.setAttribute('class', 'humidity');
+        pressure.setAttribute('class', 'pressure');
+        visibility.innerText = 'Visibility: ' + atmosphereData.visibility;
+        humidity.innerText = 'Humidity: ' + atmosphereData.humidity + '%';
+        pressure.innerText = 'Pressure: ' + atmosphereData.pressure;
+
+        atmosphereElement.appendChild(visibility);
+        atmosphereElement.appendChild(humidity);
+        atmosphereElement.appendChild(pressure);
+
+        return atmosphereElement;
+    };
+
+    Weather.prototype._createWindElement = function(data){
+        var windData = data.channel.wind;
+        var unitsData = data.channel.units;
+
+        var windElement = document.createElement('div');
+        var speed = document.createElement('span');
+
+        windElement.setAttribute('class', 'wind-wrp');
+        speed.setAttribute('class', 'wind-speed');
+        speed.innerText = 'Wind: '+ windData.speed + ' ' + unitsData.speed;
+
+        windElement.appendChild(speed);
+
+        return windElement;
+    };
 
     Weather.prototype._showWeatherOnTenDays = function(data){
         this._forecast.innerHTML = '';
